daff20342c82bb2972a1f81af4316e8bffba9990
Cristian Arias <arias.sist07@gmail.com>
2020-03-14 12:45:29 -0500

    Integrated react-bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 export default function Home(){
     return(
         <div>
             WELCOME...!
+            <Button variant="primary">Primary</Button>{' '}
         </div>
     )
 }
